7bde94cfd01cf0c9213e8f977a99962bb24db498
Ahmed Gamal <agmghazi@hotmail.com>
2020-08-25 15:47:19 +0300

    function to convet degree to decimal degree

---
+++ getDMS2DD.js
@@ -0,0 +1,12 @@
+function getDMS2DD(days, minutes, seconds, direction) {
+  direction.toUpperCase();
+  var dd = days + minutes / 60 + seconds / (60 * 60);
+  //alert(dd);
+  if (direction == "S" || direction == "W") {
+    dd = dd * -1;
+  } // Don't do anything for N or E
+  return dd;
+}
+
+console.log(getDMS2DD(38, 53, 55, "N"));
+console.log(getDMS2DD(77, 2, 16, "W"));
